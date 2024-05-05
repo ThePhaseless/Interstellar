82f469aede39cb71fe0a69edcac95738977e79e8
ThePhaseless <kukubaorch@gmail.com>
2024-05-06 00:00:16 +0200

    fix ports

--- vps_setup.sh
+++ vps_setup.sh
@@ -109,7 +109,7 @@ for port in "${REDIRECT_PORTS[@]}"; do
     nginx_config="$nginx_config
     server {
         listen $port;
-        proxy_pass $WIREGUARD_DMZ_IP:$WIREGUARD_PORT;
+        proxy_pass $WIREGUARD_DMZ_IP:$port;
     }"
 done
 nginx_config="$nginx_config
