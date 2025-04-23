44902bf21e1fe2e4aa89278278716ec58605e0cc
ThePhaseless <kukubaorch@gmail.com>
2025-04-23 13:42:16 +0200

    [skip ci] fixed deploy script and updated adguard config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-git commit -am "[Skip CI] Synced latest changes"
+git commit -am "[skip ci] Synced latest changes"
 git pull --rebase
 git push
 docker compose up -d --remove-orphans --pull always
