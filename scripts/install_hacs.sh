62fdd28c60e09c973b901a4cbdee5bdbc6026102
ThePhaseless <kukubaorch@gmail.com>
2025-05-04 21:49:26 +0200

    [skip ci] update hacs everytime

--- scripts/install_hacs.sh
+++ scripts/install_hacs.sh
@@ -3,8 +3,6 @@ set -e
 
 source .env
 
-if [ ! -d "$CONFIG_PATH"/Home/HomeAssistant/custom_components/hacs ]; then
-    command="wget -O - https://get.hacs.xyz | bash"
-    echo "$command" | docker exec -i HomeAssistant bash
-    docker restart HomeAssistant
-fi
+command="wget -O - https://get.hacs.xyz | bash"
+echo "$command" | docker exec -i HomeAssistant bash
+docker restart HomeAssistant
