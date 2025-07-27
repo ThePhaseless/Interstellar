ed4e6f55a0a3340fab82231ded8750fe14a3e047
ThePhaseless <kukubaorch@gmail.com>
2025-07-27 03:28:45 +0000

    fix encrypt sh

--- .github/workflows/encrypt.sh
+++ .github/workflows/encrypt.sh
@@ -1,3 +1,3 @@
 if [ -f "$2" ]; then
-    gpg --symmetric --cipher-algo AES256 --batch --yes --passphrase "$1" --output "$2.pem.gpg" "$2.pem"
+    gpg --symmetric --cipher-algo AES256 --batch --yes --passphrase "$1" --output "$2.gpg" "$2"
 fi
