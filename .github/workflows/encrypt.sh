cc49cc29884ffe3205f7fb1973951f2d333c706d
ThePhaseless <kukubaorch@gmail.com>
2025-07-27 03:44:45 +0000

    list files in tf

--- .github/workflows/encrypt.sh
+++ .github/workflows/encrypt.sh
@@ -1,3 +1,3 @@
 if [ -f "$2" ]; then
-    gpg --symmetric --cipher-algo AES256 --batch --yes --passphrase "$1" --output "$2.gpg" "$2"
+    gpg --symmetric --batch --passphrase "$1" --output "$2.gpg" "$2"
 fi
