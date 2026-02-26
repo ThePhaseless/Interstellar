de2a09d6ef1c2aadf3cc036d71ab61e9158ab747
ThePhaseless <kukubaorch@gmail.com>
2026-02-26 13:30:18 +0100

    chore(github): finalize secret mapping and remove obsolete code

--- Terraform/github.tf
+++ Terraform/github.tf
@@ -23,7 +23,7 @@ resource "github_actions_variable" "bws_id" {
 
   repository    = split("/", var.github_repository)[1]
   variable_name = "BWS_ID_${each.key}"
-  value         = try(local.secret_key_to_id[each.value], "placeholder")
+  value         = local.secret_key_to_id[each.value]
 }
 
 resource "github_actions_variable" "proxmox_user" {
