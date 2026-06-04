acd9016340b22aa2409045cd9c417ef4905165c8
ThePhaseless <kukubaorch@gmail.com>
2026-06-04 16:00:57 +0200

    Revert "fix(backups): exclude log files from borg archives"
    This reverts commit fc5e36cc5b9278c86d0888ede4de5341f2fd62db.

--- Kubernetes/apps/borgbackup/scripts/borg-backup.sh
+++ Kubernetes/apps/borgbackup/scripts/borg-backup.sh
@@ -104,10 +104,6 @@ while IFS= read -r target; do
         --exclude-caches \
         --exclude '*.tmp' \
         --exclude '*.log' \
-        --exclude '*.log.*' \
-        --exclude '*/log' \
-        --exclude '*/logs' \
-        --exclude 'logs.db' \
         --exclude-from "$EXCLUDE_FILE" \
         \
         ::"${prefix}-{now:%Y-%m-%dT%H:%M:%S}" \
